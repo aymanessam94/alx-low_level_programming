f9089fd3eed5836b7e7be2e77364ae0517087112
aymanessam94 <aymanessam94@gmail.com>
2023-03-16 19:46:24 -0700

    m

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Descrription: A C programm that prints stdout
+ *
+ * Return: A returns 0 (sucess)
+ */
+
+int main(void)
+{
+putchar("and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+}
