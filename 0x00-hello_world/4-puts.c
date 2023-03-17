cb150fe6cb0251e3b07e7707bb1953bb6fb1d0c6
aymanessam94 <aymanessam94@gmail.com>
2023-03-16 17:51:29 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Description: c program that prints string using puts() function
+ *
+ * Return : 0 (success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-		return(0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
