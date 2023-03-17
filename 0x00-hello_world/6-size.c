395e98d63708dc2757ea5e1523cd08da2703c538
aymanessam94 <aymanessam94@gmail.com>
2023-03-16 18:51:42 -0700

    m

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ *
+ * main -Entry Point
+ *
+ * Description : A C programm that knowsize ofdata types using sizeof() function
+ *
+ * Return: A 0 returns (success)
+ */
+
+int main(void)
+{
+	char i;
+	int j;
+	long int k;
+	long long int m;
+	float f;
+
+printf("Size of a char: %c byte(s)\n" , sizeof(i));
+printf("Size of a int: %d byte(s)\n" , sizeof(j);
+printf("Size of a long int: %ld byte(s)\n" , sizeof(k));
+printf("Size of a long long int: %lld byte(s)\n" , sizeof(m));
+printf("Size of a float: %f byte(s)\n" , sizeof(f));
+ return(0);
+}
