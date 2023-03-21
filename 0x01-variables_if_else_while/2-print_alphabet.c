b5f4fdc455a8ca8537275587486a6740b91f72a9
aymanessam94 <aymanessam94@gmail.com>
2023-03-21 14:07:44 -0700

    2nd task in C

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Dsecription: A program that check alphabet in lowercase letter
+ *
+ * Return: A 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		putchar(ch);
+		ch++;
+		putchar('\n');
+	}
+	return (0);
+}
