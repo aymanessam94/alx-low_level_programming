713261f15fad575cd512fe79da054249186bf6fa
aymanessam94 <aymanessam94@gmail.com>
2023-03-21 15:22:47 -0700

    2nd task in C

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Dsecription: A program that assign last digit to n
+ *
+ * Return: A 0 (success)
+ */
+
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+	if (ch != 'e' && ch != 'q')
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
